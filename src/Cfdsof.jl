f1271e104de7e4dbf3f78a78b408ea115d504625
andiraarif <arif@landx.id>
2022-01-27 22:55:07 +0700

    Add conduction1D case

--- src/Cfdsof.jl
+++ src/Cfdsof.jl
@@ -9,29 +9,21 @@ include("boundaryConditions.jl")
 include("output.jl")
 
 # Case general information
-caseName = "elbow"
+caseName = "conduction1D"
 
 # Mesh
 meshPath = "src/testCases/$caseName/constant/polyMesh"
 mesh = readOpenFoamMesh(meshPath)
 
 # Setup fields
-#U = setupVectorField("U", mesh, "0")
-#p = setupScalarField("p", mesh, "0")
-#T = setupScalarField("T", mesh, "0")
+T = setupScalarField("T", mesh, "0")
 
 # Boundary conditions
-#assignFixedValueBC(mesh, "hotFace", T, 300)
-#assignFixedValueBC(mesh, "airfoil", p, 10)
-#assignFixedValueBC(mesh, "empty", p, 20)
-#assignFixedValueBC(mesh, "inlet", p, 30)
-#assignFixedValueBC(mesh, "outlet", p, 40)
+assignFixedValueBC(mesh, "leftWall", T, 100)
+assignFixedValueBC(mesh, "rightWall", T, 500)
 
 # Write outputs
-#writeVtu(mesh, "src/testCases/vtu/$caseName", T)
-#writeOpenFoamMesh(mesh)
-
-printFace(mesh.faces[3])
-printFace(mesh.faces[1301])
+writeVtu(mesh, "src/testCases/vtu/$caseName", T)
+writeOpenFoamMesh(mesh)
 
 end
